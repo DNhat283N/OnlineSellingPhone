5d826b333701e40ca7b3dca0d3a1bd0c9c6527ea
DNhat283N <nhat.nd283@gmail.com>
2024-05-06 22:18:10 +0700

    Add constructor without argument for Manufacturer enity

--- OnlineSellingPhone/DataLayer/Enities/Manufacturer.cs
+++ OnlineSellingPhone/DataLayer/Enities/Manufacturer.cs
@@ -19,6 +19,8 @@ namespace DataLayer.Enities
         [MaxLength(50)]
         public string Manufacturer_Name { get; set; }
 
+        public Manufacturer() { }
+
         public Manufacturer(string manufacturer_Name)
         {
             Manufacturer_Name = manufacturer_Name;
