49c0f85b30130136634929876fd5a3b93fc561ad
DNhat283N <nhat.nd283@gmail.com>
2024-05-06 22:27:41 +0700

    Update filter by price, search phone by name, search suggestion, clear search data features and modify the UI

--- OnlineSellingPhone/WindowsFormsApp1/Mall.cs
+++ OnlineSellingPhone/WindowsFormsApp1/Mall.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing.Drawing2D;
 using BusinessLayer;
 using DataLayer.Enities;
-using System.Threading;
 using System.Globalization;
 
 namespace WindowsFormsApp1
@@ -22,12 +16,17 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
         private static int ITEM_WIDTH = 357;
         private static int ITEM_HEIGHT = 357;
         private static int MAX_COLUMNS_IN_PHONES_GRID = 3;
-        private static int HORIZONTAL_SPACING = 10;
-        private static int VERTICAL_SPACING = 10;
+        private static int HORIZONTAL_SPACING = 20;
+        private static int VERTICAL_SPACING = 20;
+
+        private static DAO.PRICE_FILTER SORT_BY = DAO.PRICE_FILTER.NONE;
+        private static string KEYWORD = "";
+        private static string MANUFACTURER_NAME = "";
+        private static List<Phone> suggestionPhoneList = new List<Phone>();
+        private List<Phone> productsByManufacturerName = new List<Phone>();
 
         private string PriceFormat(double amount)
         {
@@ -39,86 +38,146 @@ namespace WindowsFormsApp1
             CultureInfo vietnameseCulture = CultureInfo.GetCultureInfo("vi-VN");
             formattedAmount = amount.ToString("C", vietnameseCulture);
 
+            // Loại bỏ phần thập phân "00"
+            formattedAmount = formattedAmount.Replace(",00", "");
+
             return formattedAmount;
         }
 
-
-        private void SetPhoneGrid(List<Phone> phones, int itemWidth, int itemHeight, int horizontalSpacing, int verticalSpacing, int maxColumns, int offsetRowForFirstItem = 40, int offsetColumnForFirstItem = 40)
+        private void UpdateListBoxHeight(bool isDisplay)
         {
-            try
+            if (isDisplay)
             {
-                int row = 0;
-                int column = 0;
+                const int itemHeight = 16; // Độ cao mặc định của mỗi item trong ListBox
+                const int maxHeight = 300; // Độ cao tối đa của listbox
 
-                phones.ForEach(p => {
-                    Mall_Product product = new Mall_Product();
-                    Label lblName = (Label)product.Controls.Find("lblName", true).FirstOrDefault();
-                    Label lblPrice = (Label)product.Controls.Find("lblPrice", true).FirstOrDefault();
-                    PictureBox pctProduct = (PictureBox)product.Controls.Find("pcbProduct", true).FirstOrDefault();
-                    lblName.Text = p.Phone_Name;
-                    lblPrice.Text = PriceFormat(p.Phone_Price);
+                int numItems = lsbSuggestions.Items.Count;
+                int desiredHeight = Math.Min(numItems * itemHeight, maxHeight);
 
-                    // Tính toán vị trí của sản phẩm
-                    int x = 0, y = 0;
-                    if (column == 0)
-                    {
-                        x = offsetRowForFirstItem;
-                    }
-                    else
-                    {
-                        x = column * (itemWidth + horizontalSpacing) + offsetRowForFirstItem;
-                    }
-                    if (row == 0)
-                    {
-                        y = offsetColumnForFirstItem;
-                    }
-                    else
-                    {
-                        y = row * (itemHeight + verticalSpacing) + offsetColumnForFirstItem;
-                    }
-
-                    // Đặt vị trí của sản phẩm trong panel1
-                    product.Location = new Point(x, y);
-
-                    // Thêm sản phẩm vào panel1
-                    panel1.Controls.Add(product);
-
-                    // Di chuyển sang hàng và cột tiếp theo
-                    column++;
-                    if (column >= maxColumns)
-                    {
-                        column = 0;
-                        row++;
-                    }
-                });
+                lsbSuggestions.Height = desiredHeight;
             }
-            catch
+        }
+
+        private void SetPhoneGrid(List<Phone> phones, int itemWidth, int itemHeight, int horizontalSpacing, int verticalSpacing, int maxColumns, int offsetRowForFirstItem = 20, int offsetColumnForFirstItem = 20)
+        {
+            if(phones.Count() != 0)
             {
+                try
+                {
+                    int row = 0;
+                    int column = 0;
 
+                    phones.ForEach(p => {
+                        Mall_Product product = new Mall_Product();
+                        Label lblName = (Label)product.Controls.Find("lblName", true).FirstOrDefault();
+                        Label lblPrice = (Label)product.Controls.Find("lblPrice", true).FirstOrDefault();
+                        PictureBox pctProduct = (PictureBox)product.Controls.Find("pcbProduct", true).FirstOrDefault();
+                        lblName.Text = p.Phone_Name;
+                        lblPrice.Text = PriceFormat(p.Phone_Price);
+
+                        // Tính toán vị trí của sản phẩm
+                        int x = 0, y = 0;
+                        if (column == 0)
+                        {
+                            x = offsetRowForFirstItem;
+                        }
+                        else
+                        {
+                            x = column * (itemWidth + horizontalSpacing) + offsetRowForFirstItem;
+                        }
+                        if (row == 0)
+                        {
+                            y = offsetColumnForFirstItem;
+                        }
+                        else
+                        {
+                            y = row * (itemHeight + verticalSpacing) + offsetColumnForFirstItem;
+                        }
+
+                        // Đặt vị trí của sản phẩm trong panel1
+                        product.Location = new Point(x, y);
+
+                        // Thêm sản phẩm vào panel1
+                            pnlProduct.Controls.Add(product);
+
+                        // Di chuyển sang hàng và cột tiếp theo
+                        column++;
+                        if (column >= maxColumns)
+                        {
+                            column = 0;
+                            row++;
+                        }
+                    });
+                }
+                catch
+                {
+
+                }
             }
+        }
+
+        private void LoadUI()
+        {
+            ClearPanel();
+            productsByManufacturerName = DAO.QueryPhoneTableBySearchKeywordOrByManuFacturerNameAndFilterByPrice(KEYWORD, MANUFACTURER_NAME, SORT_BY);
+            SetPhoneGrid(productsByManufacturerName, ITEM_WIDTH, ITEM_HEIGHT, HORIZONTAL_SPACING, VERTICAL_SPACING, MAX_COLUMNS_IN_PHONES_GRID);
+        }
+
+        private void ShowSuggestions(string searchText)
+        {
+            suggestionPhoneList.Clear();
+
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                suggestionPhoneList = DAO.QueryPhoneTableBySearchKeywordOrByManuFacturerNameAndFilterByPrice(txtSearch.Text);
+                if (suggestionPhoneList.Count() == 0)
+                    UpdateListBoxHeight(false);
+            }
+
+            // Xóa các đề xuất cũ
+            lsbSuggestions.Items.Clear();
+
+            // Hiển thị các đề xuất mới
+            foreach (Phone suggestion in suggestionPhoneList)
+            {
+                lsbSuggestions.Items.Add(suggestion.Phone_Name);
+            }
+            UpdateListBoxHeight(true);
+            lsbSuggestions.Location = new Point(txtSearch.Left, txtSearch.Bottom + 5);
+            // Hiển thị danh sách đề xuất
+            lsbSuggestions.Visible = suggestionPhoneList.Any();
         }
 
         private void txtSearch_Enter(object sender, EventArgs e)
         {
-            if (txtSearch.Text != "")
+            if (txtSearch.Text != "" && KEYWORD == "")
             {
                 txtSearch.Text = "";
                 txtSearch.ForeColor = Color.Black;
+                lblClearSearch.Visible = false;
             }    
-
+            else if(KEYWORD != "" && txtSearch.Text != "Tìm theo tên điện thoại")
+            {
+                txtSearch.Text = KEYWORD;
+            }
         }
 
-        public void ClearPanel()
+        private void ClearPanel()
         {
-            panel1.Controls.Clear();
+            pnlProduct.Controls.Clear();
         }
 
         private void txtSearch_Leave(object sender, EventArgs e)
         {
-            if (txtSearch.Text == "")
+            if (KEYWORD == "")
             {
-                txtSearch.Text = "Tìm kiếm";
+                txtSearch.Text = "Tìm theo tên điện thoại";
                 txtSearch.ForeColor = Color.DarkGray;
+                lblClearSearch.Visible = false;
+            }
+            else if(KEYWORD != "")
+            {
+                txtSearch.Text = KEYWORD;
             }
         }
 
@@ -129,15 +188,21 @@ namespace WindowsFormsApp1
             {
                 case "Giá ↑↓":
                     btnPricefilter.Text = "Giá ↑";
+                    SORT_BY = DAO.PRICE_FILTER.ASCENDING;
                     btnPricefilter.ForeColor = Color.Red;
+                    LoadUI();
                     break;
                 case "Giá ↑":
                     btnPricefilter.Text = "Giá ↓";
+                    SORT_BY = DAO.PRICE_FILTER.DESCENDING;
                     btnPricefilter.ForeColor = Color.Red;
+                    LoadUI();
                     break;
                 case "Giá ↓":
                     btnPricefilter.Text = "Giá ↑↓";
+                    SORT_BY = DAO.PRICE_FILTER.NONE;
                     btnPricefilter.ForeColor = Color.Black;
+                    LoadUI(); 
                     break;
             }
         }
@@ -146,8 +211,7 @@ namespace WindowsFormsApp1
 
         private void Mall_Load(object sender, EventArgs e)
         {
-            List<Phone> productsByManufacturerName = DAO.QueryPhoneInformationByManufacturerName();
-            SetPhoneGrid(productsByManufacturerName, ITEM_WIDTH, ITEM_HEIGHT, HORIZONTAL_SPACING, VERTICAL_SPACING, MAX_COLUMNS_IN_PHONES_GRID);
+            LoadUI();
         }
 
         private void btnClickEvent(object sender, EventArgs e)
@@ -158,14 +222,13 @@ namespace WindowsFormsApp1
                 isButtonClicked = true;
                 foreach (Control control in Controls)
                 {
-                    if (control is Button button && button != clickedButton)
+                    if (control is Button button && button != clickedButton && button.Name.StartsWith("btn") && button.Name != "btnSearch" && button.Name != "btnPricefilter")
                     {
                         button.Enabled = false;
                     }
                 }
-                ClearPanel();
-                List<Phone> productsByManufacturerName = DAO.QueryPhoneInformationByManufacturerName(clickedButton.Text);
-                SetPhoneGrid(productsByManufacturerName, ITEM_WIDTH, ITEM_HEIGHT, HORIZONTAL_SPACING, VERTICAL_SPACING, MAX_COLUMNS_IN_PHONES_GRID);
+                MANUFACTURER_NAME = clickedButton.Text;
+                LoadUI();
             }
             else
             {
@@ -177,11 +240,49 @@ namespace WindowsFormsApp1
                         button.Enabled = true;
                     }
                 }
-                ClearPanel();
-                List<Phone> productsByManufacturerName = DAO.QueryPhoneInformationByManufacturerName();
-                SetPhoneGrid(productsByManufacturerName, ITEM_WIDTH, ITEM_HEIGHT, HORIZONTAL_SPACING, VERTICAL_SPACING, MAX_COLUMNS_IN_PHONES_GRID);
+                MANUFACTURER_NAME = "";
+                LoadUI();
             }
         }
 
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lsbSuggestions.Visible = false;
+            if (txtSearch.Text != "" && txtSearch.Text != "Tìm theo tên điện thoại")
+            {
+                KEYWORD = txtSearch.Text;
+                LoadUI();
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            ShowSuggestions(txtSearch.Text);
+            if(txtSearch.Text != "" && txtSearch.Text != "Tìm theo tên điện thoại")
+            {
+                KEYWORD = txtSearch.Text;
+                lblClearSearch.Location = new Point(txtSearch.Left - lblClearSearch.Width - 5, txtSearch.Top);
+                lblClearSearch.Visible = true;
+            }
+        }
+
+        private void lblClearSearch_Click(object sender, EventArgs e)
+        {
+            KEYWORD = "";
+            txtSearch.Text = KEYWORD;
+            lblClearSearch.Visible = false;
+            LoadUI();
+        }
+
+        private void lsbSuggestions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsbSuggestions.SelectedItem != null)
+            {
+                string selectedSuggestion = lsbSuggestions.SelectedItem.ToString();
+                lsbSuggestions.Text = selectedSuggestion;
+                txtSearch.Text = selectedSuggestion;
+                lsbSuggestions.Visible = false; // Ẩn danh sách đề xuất khi người dùng chọn một đề xuất
+            }
+        }
     }
 }
