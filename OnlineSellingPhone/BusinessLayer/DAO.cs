57bba69e1f6bdef6c097188211a81e762fe1207e
DNhat283N <nhat.nd283@gmail.com>
2024-05-22 22:49:04 +0700

    Modify AddPhone, Login and some private methods

--- OnlineSellingPhone/BusinessLayer/DAO.cs
+++ OnlineSellingPhone/BusinessLayer/DAO.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System;
 using DataLayer.Enities;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace BusinessLayer
 {
@@ -76,7 +75,7 @@ namespace BusinessLayer
                 db.PhoneNumbers.Add(new PhoneNumber(phoneNumber, customer.Customer_ID));
                 db.SaveChanges();
             }
-        }   
+        }
 
         public static bool IsExistAccount(string username)
         {
@@ -141,7 +140,7 @@ namespace BusinessLayer
 
         //Sign In Form
 
-        public static bool AuthenticateAccountLogin(string username, string password)
+        private static bool AuthenticateAccountLogin(string username, string password)
         {
             bool isAuthenticated = false;
             using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
@@ -159,12 +158,35 @@ namespace BusinessLayer
             return isAuthenticated;
         }
 
+        public static int AuthenticateAndAuthorizeAccountLogin(string username, string password)
+        {
+            int role = -1;
+            if (AuthenticateAccountLogin(username, password))
+            {
+                using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
+                {
+                    var query = from acc in db.Accounts
+                                where (acc.Account_Username == username.ToLower() && acc.Account_Password == password)
+                                select acc.Account_UserRoleEnum;
+                    UserRoleEnum userRole = query.FirstOrDefault();
+                    if (userRole == UserRoleEnum.ADMIN)
+                    {
+                        role = 0;
+                    }
+                    else if (userRole == UserRoleEnum.CUSTOMER)
+                        role = 1;
+                }
+            }
+            else role = -1;
+            return role;
+        }
+
         //Query 
 
         private static List<Phone> QueryPhoneInformationByManufacturerName(List<Phone> phones, string name = "")
         {
             List<Phone> resultPhonesList = new List<Phone>(phones);
-            if(name != "")
+            if (name != "")
             {
                 using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
                 {
@@ -184,12 +206,12 @@ namespace BusinessLayer
         private static List<Phone> SortPhoneList(List<Phone> phones, PRICE_FILTER sort = PRICE_FILTER.NONE)
         {
             List<Phone> sortedPhoneList = new List<Phone>(phones);
-            if(sort == PRICE_FILTER.ASCENDING)
+            if (sort == PRICE_FILTER.ASCENDING)
             {
                 sortedPhoneList.Sort((x, y) => x.Phone_Price.CompareTo(y.Phone_Price));
                 return sortedPhoneList;
-            } 
-            else if(sort == PRICE_FILTER.DESCENDING)
+            }
+            else if (sort == PRICE_FILTER.DESCENDING)
             {
                 sortedPhoneList.Sort((x, y) => y.Phone_Price.CompareTo(x.Phone_Price));
                 return sortedPhoneList;
@@ -201,7 +223,7 @@ namespace BusinessLayer
         private static List<Phone> SearchPhone(string keyword = "")
         {
             List<Phone> phoneList = new List<Phone>();
-            if(keyword == "")
+            if (keyword == "")
             {
                 phoneList = QueryAllPhoneTable();
             }
@@ -210,20 +232,38 @@ namespace BusinessLayer
                 using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
                 {
                     var query = from p in db.Phones
-                            where p.Phone_Name.ToLower().Contains(keyword.ToLower())
-                            select p;
+                                where p.Phone_Name.ToLower().Contains(keyword.ToLower())
+                                select p;
                     phoneList = query.ToList();
                 }
             }
             return phoneList;
         }
 
-        public static List<Phone> QueryPhoneTableBySearchKeywordOrByManuFacturerNameAndFilterByPrice(string keyword = "", string manufacturerName = "", PRICE_FILTER priceFilter = PRICE_FILTER.NONE)
+        private static List<Phone> ShowInactivatingPhone(List<Phone> phones, bool isShowInactivatingPhone)
+        {
+            List<Phone> phoneList = new List<Phone>(phones);
+            if (!isShowInactivatingPhone)
+            {
+                using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
+                {
+                    var query = from p in db.Phones
+                                where p.Phone_Active.Equals(true)
+                                select p;
+                    phoneList = query.ToList();
+                }
+                return phoneList;
+            }
+            else return phoneList;
+        }
+
+        public static List<Phone> QueryPhoneTableBySearchKeywordOrByManuFacturerNameAndFilterByPrice(string keyword = "", string manufacturerName = "", PRICE_FILTER priceFilter = PRICE_FILTER.NONE, bool isShowInactivatedPhone = false)
         {
             List<Phone> resultListPhone = new List<Phone>();
             resultListPhone = SearchPhone(keyword);
             resultListPhone = QueryPhoneInformationByManufacturerName(resultListPhone, manufacturerName);
             resultListPhone = SortPhoneList(resultListPhone, priceFilter);
+            resultListPhone = ShowInactivatingPhone(resultListPhone, isShowInactivatedPhone);
             return resultListPhone;
         }
 
@@ -239,19 +279,16 @@ namespace BusinessLayer
             return data;
         }
 
-        public static List<Phone> QueryPhoneWithConfiguration()
+        public static Phone QueryPhoneByID(int phoneID)
         {
-            List<Phone> list = new List<Phone>();
             using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
             {
                 var query = from p in db.Phones
-                            join pc in db.PhoneConfigurations on p.Phone_ID equals pc.Phone_ID
-                            join c in db.Configurations on pc.Configuration_ID equals c.Configuration_ID
-                            join m in db.Manufacturers on p.Manufacturer_ID equals m.Manufacturer_ID
+                            join c in db.Configurations on p.Configuration_ID equals c.Configuration_ID
+                            where p.Phone_ID.Equals(phoneID)
                             select p;
-                list = query.ToList();
+                return query.FirstOrDefault();
             }
-                return list;
         }
 
         public static List<Image> QueryImagesOfPhoneByPhoneID(int phoneID)
@@ -270,64 +307,6 @@ namespace BusinessLayer
 
         //Admin
 
-        private static bool AddImage(string[] imageURLs, int phoneID)
-        {
-            bool isImageAdded = false;
-            using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
-            {
-                if (db.Phones.Count(p => p.Phone_ID == phoneID) != 1)
-                {
-                    return isImageAdded = false;
-                }
-
-                int countImagesOfPhone = db.Images.Count(i => i.Phone_ID == phoneID);
-                if (countImagesOfPhone >= 0 || countImagesOfPhone < 2)
-                {
-                    foreach (string url in imageURLs)
-                    {
-                        db.Images.Add(new Image(url, phoneID));
-                    }
-                    isImageAdded = true;
-                }
-                else
-                {
-                    return isImageAdded = false;
-                }
-
-            }
-            return isImageAdded;
-        }
-
-
-        public static bool AddPhone(string phoneName, string phoneColor, double phonePrice, int phoneReadyInStock, int phoneManufacturerID, string [] phoneImageURLs)
-        {
-            bool isAdded = false;
-            if (phoneImageURLs.Length > 2)
-            {
-                Console.WriteLine("Error Of Array Length!");
-                return isAdded;
-            }
-            try
-            {
-                using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
-                {
-                    Phone phone = new Phone(phoneName, phoneColor, phonePrice, phoneReadyInStock, phoneManufacturerID);
-                    db.Phones.Add(phone);                   
-                    if (AddImage(phoneImageURLs, phone.Phone_ID)){
-                        db.SaveChanges();
-                        isAdded = true;
-                    }
-                }
-            }
-            catch
-            {
-                isAdded = false;
-            }
-            return isAdded;
-        }
-
-        //Admin form
-        
         private static bool IsExsitPhoneConfig(int RAM, int ROM)
         {
             using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
@@ -339,7 +318,7 @@ namespace BusinessLayer
             }
         }
 
-        private static int SelectConfigurationIDFromRAMAndROM(int RAM, int ROM)
+        private static int GetConfigurationIDFromRAMAndROM(int RAM, int ROM)
         {
             if (IsExsitPhoneConfig(RAM, ROM))
             {
@@ -357,18 +336,39 @@ namespace BusinessLayer
                 return -1;
         }
 
-        public static void AddPhone(string phoneName, int manufacturerID, string phoneColor, double phonePrice, int phoneQuantityInStock, string imageURL1, int RAM, int ROM, string imageURL2 =  null)
+        public static int GetQuantityOfPhoneByID(int phoneID)
         {
             using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
             {
-                Phone p = new Phone(phoneName, phoneColor, phonePrice, phoneQuantityInStock, manufacturerID);
-                db.Phones.Add(p);
+                if (db.Phones.Any(p => p.Phone_ID == phoneID))
+                    return -1;
+                else
+                {
+                    var query = from p in db.Phones
+                                where p.Phone_ID == phoneID
+                                select p.Phone_ReadyInStock;
+                    return query.FirstOrDefault();
+                }
+            }
+        }
 
-                if(IsExsitPhoneConfig(RAM, ROM)){
-                    int configID = SelectConfigurationIDFromRAMAndROM(RAM, ROM);
+        public static void AddPhone(string phoneName, int manufacturerID, string phoneColor, double phonePrice, int phoneQuantityInStock, string imageURL1, int RAM, int ROM, string imageURL2 = null, bool isActive = true)
+        {
+            using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
+            {
+                if (IsExsitPhoneConfig(RAM, ROM))
+                {
+                    int configID = GetConfigurationIDFromRAMAndROM(RAM, ROM);
                     if (configID > 0)
                     {
-                        db.PhoneConfigurations.Add(new PhoneConfiguration(p.Phone_ID, configID));
+                        Phone p = new Phone(phoneName, phoneColor, phonePrice, phoneQuantityInStock, manufacturerID, configID, isActive);
+                        db.Phones.Add(p);
+
+                        db.Images.Add(new Image(imageURL1, p.Phone_ID));
+                        if (imageURL2 != "")
+                        {
+                            db.Images.Add(new Image(imageURL2, p.Phone_ID));
+                        }
                     }
                     else
                         return;
@@ -377,10 +377,81 @@ namespace BusinessLayer
                 {
                     Configuration config = new Configuration(RAM, ROM);
                     db.Configurations.Add(config);
-                    db.PhoneConfigurations.Add(new PhoneConfiguration(p.Phone_ID, config.Configuration_ID));
+
+                    Phone p = new Phone(phoneName, phoneColor, phonePrice, phoneQuantityInStock, manufacturerID, config.Configuration_ID, isActive);
+                    db.Phones.Add(p);
+
+                    db.Images.Add(new Image(imageURL1, p.Phone_ID));
+                    if (imageURL2 != "")
+                    {
+                        db.Images.Add(new Image(imageURL2, p.Phone_ID));
+                    }
                 }
 
                 db.SaveChanges();
+            }
+        }
+
+        private static List<Image> GetImagesOfPhoneByPhoneID(int phoneID)
+        {
+            List<Image> images = new List<Image>();
+            using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
+            {
+                if (db.Phones.Any(p => p.Phone_ID == phoneID))
+                {
+                    var query = from i in db.Images
+                                where i.Phone_ID == phoneID
+                                select i;
+                    images = query.ToList();
+                }
+            }
+            return images;
+        }
+
+        public static bool UpdaePhoneInformation(int phoneID, string phoneName, int manufacturerID, string phoneColor, double phonePrice, int phoneQuantityInStock, string imageURL1, int RAM, int ROM, string imageURL2 = null, bool isActive = true)
+        {
+            bool isUpdated = false;
+            using (OnlineSellingPhoneContext db = new OnlineSellingPhoneContext())
+            {
+                if (db.Phones.Any(p => p.Phone_ID == phoneID))
+                {
+                    Phone phone = db.Phones.FirstOrDefault(p => p.Phone_ID == phoneID);
+                    phone.Phone_Name = phoneName;
+                    phone.Phone_Price = phonePrice;
+                    phone.Phone_Color = phoneColor;
+                    phone.Phone_ReadyInStock = phoneQuantityInStock;
+                    phone.Phone_Active = isActive;
+                    phone.Manufacturer_ID = manufacturerID;
+
+                    int configID = GetConfigurationIDFromRAMAndROM(RAM, ROM);
+                    if (configID >= 0)
+                    {
+                        if (configID != phone.Configuration_ID)
+                            phone.Configuration_ID = configID;
+                    }
+
+                    List<Image> images = new List<Image>();
+                    images = GetImagesOfPhoneByPhoneID(phone.Phone_ID);
+
+                    if (images != null && images.Count > 0)
+                    {
+                        if (images.Count > 0 && !string.IsNullOrEmpty(imageURL1))
+                        {
+                            images[0].Image_URL = imageURL1;
+                            db.Entry(images[0]).State = System.Data.Entity.EntityState.Modified;
+                        }
+
+                        if (images.Count > 1)
+                        {
+                            images[1].Image_URL = imageURL2;
+                            db.Entry(images[1]).State = System.Data.Entity.EntityState.Modified;
+                        }
+                    }
+                    db.SaveChanges();
+                    isUpdated = true;
+                }
+                else isUpdated = false;
+                return isUpdated;
             }
         }
     }
